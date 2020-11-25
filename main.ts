0fd6f424d993cc37678592e037eec9fe39f4a71a
Pierre Lachance <32958528+pierrelachance@users.noreply.github.com>
2020-11-25 15:50:33 -0500

    Ajout de A+B pour une danse complète

--- main.ts
+++ main.ts
@@ -1,5 +1,52 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 11; index++) {
+    	
+    }
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . # . . .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . . # .
+        . . # . #
+        . # . . #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
         basic.showLeds(`
             # . # . #
             . # # # .
